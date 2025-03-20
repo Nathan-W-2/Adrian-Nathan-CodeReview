cdbb058c976c3e9f0a564a77dcae56919ce7e7b8
Nathan-W-2 <nwilliams2@Carthage.edu>
2025-03-20 15:03:33 -0500

    Added LCM helper function
    Also changed parameters to be more descriptive

--- main.cpp
+++ main.cpp
@@ -6,11 +6,13 @@ using namespace std;
 bool characteristic(const char numString[], int& c);
 bool mantissa(const char numString[], int& numerator, int& denominator);
 
-bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
-bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len); 
+bool add(int characteristic1, int numerator1, int denominator1, int characteristic2, int numerator2, int denominator2, char result[], int len);
+bool subtract(int characteristic1, int numerator1, int denominator1, int characteristic2, int numerator2, int denominator2, char result[], int len); 
 
-bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
-bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+bool multiply(int characteristic1, int numerator1, int denominator1, int characteristic2, int numerator2, int denominator2, char result[], int len);
+bool divide(int characteristic1, int numerator1, int denominator1, int characteristic2, int numerator2, int denominator2, char result[], int len);
+
+int LCM(int a, int b);
 
 int main()
 {
@@ -35,20 +37,20 @@ int main()
 
     //room for 9 characters plus the null terminating character
     char answer[10];
-    int c1, n1, d1;
-    int c2, n2, d2;
+    int characteristic1, numerator1, denominator1;
+    int characteristic2, numerator2, denominator2;
 
     //initialize the values
-    c1 = 1;
-    n1 = 1;
-    d1 = 2;
+    characteristic1 = 1;
+    numerator1 = 1;
+    denominator1 = 2;
 
-    c2 = 2;
-    n2 = 2;
-    d2 = 3; 
+    characteristic2 = 2;
+    numerator2 = 2;
+    denominator2 = 3; 
 
     //if the c-string can hold at least the characteristic
-    if(add(c1, n1, d1, c2, n2, d2, answer, 10))
+    if(add(characteristic1, numerator1, denominator1, characteristic2, numerator2, denominator2, answer, 10))
     {
         //display string with answer 4.1666666 (cout stops printing at the null terminating character)
         cout<<"Answer: "<<answer<<endl;
@@ -59,7 +61,7 @@ int main()
         cout<<"Error on add"<<endl;
     }
 
-    if(divide(c1, n1, d1, c2, n2, d2, answer, 10))
+    if(divide(characteristic1, numerator1, denominator1, characteristic2, numerator2, denominator2, answer, 10))
     {
         //display string with answer
         cout<<"Answer: "<<answer<<endl;
@@ -88,10 +90,12 @@ bool mantissa(const char numString[], int& numerator, int& denominator)
     return true;
 }
 //--
-bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+bool add(int characteristic1, int numerator1, int denominator1, int characteristic2, int numerator2, int denominator2, char result[], int len)
 {
-    //you will have to come up with an algorithm to add the two numbers
-    //hard coded return value to make the main() work
+    //if denominators are different, we must cross multiply the fractions
+    if (denominator1 != denominator2) {
+        cout << LCM(denominator1, denominator2) << endl;
+    }
     result[0] = '4';
     result[1] = '.';
     result[2] = '1';
@@ -106,21 +110,21 @@ bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
     return true;
 }
 //--
-bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+bool subtract(int characteristic1, int numerator1, int denominator1, int characteristic2, int numerator2, int denominator2, char result[], int len)
 {
     //hard coded return value to make the code compile
     //you will have to come up with an algorithm to subtract the two numbers
     return true;
 }
 //--
-bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+bool multiply(int characteristic1, int numerator1, int denominator1, int characteristic2, int numerator2, int denominator2, char result[], int len)
 {
     //hard coded return value to make the code compile
     //you will have to come up with an algorithm to multiply the two numbers
     return true;
 }
 //--
-bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+bool divide(int characteristic1, int numerator1, int denominator1, int characteristic2, int numerator2, int denominator2, char result[], int len)
 {
     //you will have to come up with an algorithm to divide the two numbers
     //hard coded return value to make the main() work
@@ -133,4 +137,15 @@ bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int l
     result[6] = '\0';
     
     return true;
+}
+
+//takes two ints and returns their least common multiple 
+int LCM(int a, int b)
+{
+    int greater = max(a, b);
+    int smallest = min(a, b);
+    for (int i = greater; ; i += greater) {
+        if (i % smallest  == 0)
+            return i;
+    }
 }
