88e294ad1092726bbdf8269c097b68dc8a679527
Adrian Grady <54862578+Adrian-Grady@users.noreply.github.com>
2025-03-26 02:04:55 -0500

    Delete InformalCodeReview.h

--- InformalCodeReview.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-using namespace std;
-class InformalCodeReview
-{
-public:
-	bool characteristic(const char numString[], int& c);
-	bool mantissa(const char numString[], int& numerator, int& denominator);
-};
